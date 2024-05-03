f995259933486c9a1013da9b89a4a36c90963b95
Mat Swatek <azcards32@gmail.com>
2024-05-03 14:48:37 -0700

    Update lockout24.py

--- lockout24.py
+++ lockout24.py
@@ -156,7 +156,7 @@ for team in league.standings():
 for transaction in league.transactions():
     Type = transaction.type
     st.write(f"#{transaction.type}\t{transaction.status}\t"
-          f"({transaction.players.player.name})")
+          f"({transaction.players.name})")
 '''    
 
 ##### Create Matchup Variable #####
