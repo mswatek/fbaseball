284841796cf3b02d81c9ffdb163d1fe6dc96e8f6
Mat Swatek <azcards32@gmail.com>
2024-05-14 13:49:34 -0700

    Update lockout24.py

--- lockout24.py
+++ lockout24.py
@@ -122,7 +122,7 @@ except Exception:
     league: list = ctx.get_leagues("mlb", 2024)[0]
 
 
-st.info("this is just a test!!!!")
+st.info("this is just a test!!!!",icon="chart_with_upwards_trend")
 
 
 for transaction in league.transactions():
