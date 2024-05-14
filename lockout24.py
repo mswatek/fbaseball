fc9306f77659cb6a83354cab032b512bd5053f71
Mat Swatek <azcards32@gmail.com>
2024-05-14 12:55:57 -0700

    Update lockout24.py

--- lockout24.py
+++ lockout24.py
@@ -123,7 +123,7 @@ except Exception:
 
 
 for transaction in league.transactions():
-    st.write(transaction.players.player.name.last)
+    st.write(transaction.players)
 
 '''
 ##### BRING IN ALL WEEKS #####
