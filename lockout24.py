97307688fc6731b460d82de068a0f2866e3b4ee4
Mat Swatek <azcards32@gmail.com>
2024-05-16 14:15:25 -0700

    Update lockout24.py

--- lockout24.py
+++ lockout24.py
@@ -135,7 +135,7 @@ for transaction in league.transactions():
         test = transaction.players.player
         df = pd.DataFrame({"Player":test.name.first+" "+test.name.last,"Team":test.editorial_team_abbr,"Position":test.display_position,"Time":transaction.timestamp,"Type":test.transaction_data.source_type,"Manager":test.transaction_data.destination_team_name}, index=[0])
     else: 
-        df = pd.DataFrame({"Player":1,"Team":1,"Position":1,"Time":1,"Type":1,"Manager":1}, index=[0])
+        df = pd.DataFrame({"Player":1,"Team":1,"Position":1,"Time":1,"Type":1,"Manager":1}) #, index=[0]
     #st.write(f"{test.name.first}\t{test.name.last}\t{test.editorial_team_abbr}\t{test.display_position}\t{test.transaction_data.type}\t{test.transaction_data.source_type}\t{test.transaction_data.source_team_key}\t")
     #st.write(f"{test.name.first}\t{test.name.last}\t{test.editorial_team_abbr}\t{test.display_position}\t{test.transaction_data.type}\t{test.transaction_data.source_type}\t{test.transaction_data.destination_team_name}\t")
     #df = pd.DataFrame({"First":test.name.first,"Last":test.name.last,"Team":test.editorial_team_abbr,"Position":test.display_position}, index=[0])
