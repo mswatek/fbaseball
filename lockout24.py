bfe10d981837e187366126cd5dae6f0285ea84cc
Mat Swatek <azcards32@gmail.com>
2024-05-28 15:12:00 -0700

    Update lockout24.py

--- lockout24.py
+++ lockout24.py
@@ -246,7 +246,7 @@ all_weeks['PA'] = (all_weeks['OBP']*all_weeks['AB']-all_weeks['H'])/(1-all_weeks
 all_weeks['OnBase'] = all_weeks['OnBase'].astype(int)
 all_weeks['PA'] = all_weeks['PA'].astype(int)
 
-all_weeks['test']  = all_weeks['OnBase']/all_weeks['PA']
+all_weeks['test']  = all_weeks['OnBase']/all_weeks['PA'] - all_weeks['OBP']
 
 st.write(all_weeks)
 
